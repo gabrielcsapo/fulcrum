25183db4e2c600d9ab1a0bc466163d7bbe210a3e
Gabriel Csapo <gabecsapo@gmail.com>
2021-10-14 22:54:24 -0700

    [chore] rollback dependency upgrade, use hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
